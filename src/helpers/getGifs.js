b0ff9b5534b7de7f4b9888c9d7fd124fb25b63ad
Jorge García <jlgarcia-@hotmail.com>
2021-09-07 10:24:24 -0600

    Fist commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+
+
+export const getGifs = async( category ) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=Irifqab3H4E8lcu1m66aFoj5USd63f9q`;
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+   return gifs;
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GifExpetApp } from './GifExpetApp';
+
+import './index.css';
+
+ReactDOM.render(
+    <GifExpetApp />,
+  document.getElementById('root')
+);
+
+
